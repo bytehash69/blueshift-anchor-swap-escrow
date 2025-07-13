2bdcb77299a9aeaf35b231418bd8b08ce05f3a4c
Adiflickzzz <adivardhan2248@gmail.com>
2025-07-13 14:26:28 +0530

    Ix - Refund

---
+++ programs/blueshift_anchor_escrow/src/instructions/refund.rs
@@ -0,0 +1,100 @@
+#![allow(unexpected_cfgs)]
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{
+        close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface,
+        TransferChecked,
+    },
+};
+
+use crate::{errors::EscrowError, state::Escrow};
+
+#[derive(Accounts)]
+pub struct Refund<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+    #[account(
+        mut,
+        close = maker,
+        seeds = ["escrow".as_bytes(), maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
+        bump = escrow.bump,
+        has_one = maker @ EscrowError::InvalidMaker,
+        has_one = mint_a @ EscrowError::InvalidMintA,
+    )]
+    pub escrow: Account<'info, Escrow>,
+
+    /// Token Accounts
+    pub mint_a: InterfaceAccount<'info, Mint>,
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority = escrow,
+        associated_token::token_program = token_program
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        init_if_needed,
+        payer = maker,
+        associated_token::mint = mint_a,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+    )]
+    pub maker_ata_a: InterfaceAccount<'info, TokenAccount>,
+
+    /// Programs
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Refund<'info> {
+    fn withdraw_and_close_vault(&mut self) -> Result<()> {
+        // Create the signer seeds for the Vault
+        let signer_seeds: [&[&[u8]]; 1] = [&[
+            b"escrow",
+            self.maker.to_account_info().key.as_ref(),
+            &self.escrow.seed.to_le_bytes()[..],
+            &[self.escrow.bump],
+        ]];
+
+        let cpi_context = CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            TransferChecked {
+                from: self.vault.to_account_info(),
+                to: self.maker_ata_a.to_account_info(),
+                mint: self.mint_a.to_account_info(),
+                authority: self.escrow.to_account_info(),
+            },
+            &signer_seeds,
+        );
+
+        // Transfer Token A (Vault -> Maker)
+        transfer_checked(
+           cpi_context, 
+            self.vault.amount,
+            self.mint_a.decimals,
+        )?;
+
+        let close_context = CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            CloseAccount {
+                account: self.vault.to_account_info(),
+                authority: self.escrow.to_account_info(),
+                destination: self.maker.to_account_info(),
+            },
+            &signer_seeds,
+        );
+        // Close the Vault
+        close_account(close_context)?;
+
+        Ok(())
+    }
+}
+
+pub fn handler(ctx: Context<Refund>) -> Result<()> {
+    // Withdraw and close the Vault (Vault -> Maker)
+    ctx.accounts.withdraw_and_close_vault()?;
+
+    Ok(())
+}
